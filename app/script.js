14c02eca472efdbdd97867a896ea2ea7d202f649
IsaacMRodrigues <isaacmrodrigues2019@gmail.com>
2024-03-07 21:13:16 -0300

    mudanças nos comentarios

--- app/script.js
+++ app/script.js
@@ -5,8 +5,9 @@ const submitComment = (event) => {
 
     const author = inputAuthor.value;
     const comment = inputComment.value;
+    const dat = new Date();
 
-    data.push({ author: author, comment: comment })
+    data.push({ author: author, comment: comment, dat: dat })
     console.log(data)
 
     loadComment()
@@ -18,24 +19,28 @@ formComentario.addEventListener("submit", submitComment)
 const loadComment = () => {
     // Dados carregados da API
     if (data) {
-        //displayComment();
+        displayComment();
     }
 }
 
 const displayComment = () => {
-    const body = document.getElementsByTagName('body')[0];
-
-    data.forEach(elemento => {
+    const divFeed = document.getElementById('comment-feed');
+    divFeed.innerHTML = ``
+    data.forEach(item => {
         const divDisplay = document.createElement('div');
         divDisplay.className = 'comentarios'
         divDisplay.innerHTML = `
-        <strong>
-        <span class="badge text-bg-secondary">${elemento.author}</span>
-        </strong>
-        <p>${elemento.comment}</p>
+        <div class="cardzin">
+        <div class="d-flex">
+            <p class="pb-0 mb-0 small lh-sm border-bottom">
+                <strong class="d-block">@${item.author}</strong>
+                ${item.comment}     
+            </p>
+            
+        </div>
+        <small class="d-block text-end">${item.dat.toLocaleDateString()} as ${item.dat.getHours()}:${item.dat.getMinutes()}</small>
+        </div>
         `
-        body.appendChild(divDisplay);
+        divFeed.appendChild(divDisplay);
     })
-
-
 }
